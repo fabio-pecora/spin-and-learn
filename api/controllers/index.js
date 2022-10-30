29cd9f63fbb746b0ced404d71b4d48aafb5359d6
fabio-pecora <fabiopecora01@gmail.com>
2022-10-30 11:26:11 -0400

    users-api

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 // Load each controller
 const microPostsController = require("./microPosts.js");
+const usersController = require("./users.js");
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use("/micro_posts", microPostsController);
+router.use("/users", usersController);
 
 module.exports = router;
