c02d1061e062429c6e8822ac8f5609f5024966cd
Ahmai Chaney-Smith <ahmai.chaney-smith@macaulay.cuny.edu>
2022-11-30 19:31:25 -0500

    sign up page done.

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -3,8 +3,21 @@ import React from 'react';
 function SignUpPage(props) {
     return (
         <>
-            <div>
-                <p>sign up page content</p>
+            <div id="graySide">
+            </div>
+            <div id="redSide">
+                <div id="signUpDiv1">
+                    <p>CREATE ACCOUNT</p>
+                </div>
+                <div id="signUpDiv2">
+                    <form>
+                        <input type="text" id="signUpFirstName" name="signUpFirstName" placeholder='FIRST NAME'></input>
+                        <input type="text" id="signUpLastName" name="signUpLastName" placeholder='LAST NAME'></input>
+                        <input type="text" id="signUpEmail" name="signUpEmail" placeholder='EMAIL'></input>
+                        <input type="text" id="signUpPassword" name="signUpPassword" placeholder='PASSWORD'></input>
+                        <input type="submit" id="signUpSubmit" value="SIGN UP"></input>
+                    </form>
+                </div>
             </div>
         </>
     );
